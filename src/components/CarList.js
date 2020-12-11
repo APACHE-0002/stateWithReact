b5f45187d04612c9364a69d6343d039cf9a4a2f7
APACHE-0002 <vinyy64@live.com>
2020-12-10 22:41:40 -0300

    fist commit

---
+++ src/components/CarList.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './car.css';
+
+class CarList extends Component{
+  state = {
+    newCar: '',
+    cars: [
+      'chevette',
+      'camaro ss',
+      'brasilia'
+    ]
+  }
+
+  handleInputChange = e => {
+    this.setState({ newCar: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      cars: [...this.state.cars, this.state.newCar],
+      newCar: '',
+    })
+  }
+
+  handleDelete = e =>{
+    this.setState({ cars: this.state.cars.filter(c => c !== e)})
+  }
+
+  render(){
+    return(
+      <>
+      <nav>
+       <div class="item">
+        <input type="checkbox" id="check1"/>
+        <label for="check1">Carros</label>
+        <ul>
+            {this.state.cars.map(car => 
+            <li key={car}><a>{car}</a>
+            <button onClick={() => this.handleDelete(car)}>excluir</button>
+            </li>
+            )}
+        </ul> 
+       </div>
+    </nav>
+
+    <form onSubmit={this.handleSubmit}>
+    <input type="text" onChange={this.handleInputChange}/>
+    <button type="submit">Enviar</button>
+    </form>
+
+    </>
+    )
+  }
+
+}
+
+export default CarList;
+
